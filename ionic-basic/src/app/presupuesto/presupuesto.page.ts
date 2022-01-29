a9a76bed2bb8fa14bfe430b148762ff5de2b6e46
Alzero7 <jessar.alzero.7@gmail.com>
2022-01-29 03:25:27 -0600

    Practica 01

---
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-presupuesto',
+  templateUrl: './presupuesto.page.html',
+  styleUrls: ['./presupuesto.page.scss'],
+})
+export class PresupuestoPage implements OnInit {
+
+  public gastos: string[]=['Directos','Fijos', 'Variables'];
+  public selectedValue: any;
+  public monto: number;
+  public resultados: string;
+  public errResultados: string='light';
+
+  constructor() { 
+    
+  }
+
+customPopoverOptions: any = {
+    header: 'Seleccion de gasots',
+    subHeader: 'Seleccione el tipo de gasto',
+    message: 'Solo seleccione un tipo de gasto'
+  };
+
+  cambioValor(value){
+    console.log(value);
+  }
+  
+  guardar(){
+    this.resultados ="";
+    if(this.monto!=null && this.selectedValue!=null){
+      this.errResultados = 'success';
+      this.resultados = 'Gasto seleccionado: '+this.selectedValue+' \nMonto: '+this.monto+'\n';
+    }
+    else{
+      this.errResultados = 'danger';
+      this.resultados ="No a completado los campos del formulario";
+    }
+  }
+
+
+  ngOnInit() {
+  }
+
+}
