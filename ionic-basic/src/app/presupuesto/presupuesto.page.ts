12df88f45cdb88d488fdd4d6558c09da4eff33c2
Fran22Espinoza <fespinozagal@gmail.com>
2022-02-10 19:28:53 -0600

    practica 02

--- ionic-basic/src/app/presupuesto/presupuesto.page.ts
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -1,5 +1,6 @@
-import { NumberFormatStyle } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Gasto } from '../models/gastos.models';
+import { GastosService } from '../services/gastos.service';
 
 @Component({
   selector: 'app-presupuesto',
@@ -8,15 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresupuestoPage implements OnInit {
 
-  public gastos: string[]=['Directos','fijos','variables'];
-  public selectvalue: any;
-  public monto: Number;
+  public gastos: string[]=['Directos','Fijos', 'Variables'];
+  public selectedValue: any;
+  public monto: number;
   public resultados: string;
   public errResultados: string='light';
-
-  constructor() { }
-
   
+  public gasotsList: Gasto[]=[];
+  public descripcion: string;
+  public tipoGasto: string='';
+
+  constructor(private gastosService: GastosService) {
+    
+   }
+
+   customPopoverOptions: any = {
+    header: 'Seleccion de gasots',
+    subHeader: 'Seleccione el tipo de gasto',
+    message: 'Solo seleccione un tipo de gasto'
+  };
+
+  cambioValor(value){
+    console.log(value);
+  }
+  
+  guardar(){
+    this.resultados ="";
+    if(this.monto!=null && this.selectedValue!=null && this.descripcion!= null){
+      this.errResultados = 'success';
+      this.resultados = 'Gasto seleccionado: '+this.selectedValue+' \nMonto: '+this.monto+'\n'+
+      'Descricion: '+this.descripcion;
+      let gasto: Gasto = {
+      descripcion: this.descripcion,
+      tipo: this.selectedValue,
+      monto: this.monto
+    }
+      this.gastosService.agregar(gasto);
+      this.gasotsList = this.gastosService.getGastos();
+    }
+    else{
+      this.errResultados = 'danger';
+      this.resultados ="No a completado los campos del formulario";
+    }
+  }
+
+  borrarGasto(idGasto: number){
+    this.gastosService.borrarGasto(idGasto);
+    this.gasotsList = this.gastosService.getGastos();
+  }
+
   ngOnInit() {
   }
 
--- ionic-basic/src/app/services/gastos.service.ts
+++ ionic-basic/src/app/services/gastos.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Gasto } from '../models/gastos.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GastosService {
 
+  gastos: Gasto[]=[];
+  
   constructor() { }
+  agregar(gasto: Gasto){
+    if(this.gastos.length==0){
+      gasto.id = 1;
+    } else{
+      gasto.id = this.gastos.length + 1;
+    }
+    this.gastos.push(gasto);
+  }
+
+  borrarGasto(idGasto: number){
+    this.gastos = 
+    this.gastos.filter((gasto)=>gasto.id!=idGasto);
+  }
+
+  getGastos(){
+    return this.gastos;
+  }
 }
