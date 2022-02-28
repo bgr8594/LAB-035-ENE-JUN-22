931a93287729726ff0ee93129c83c4cd0dc73bfe
Alzero7 <jessar.alzero.7@gmail.com>
2022-02-27 23:02:44 -0600

    Practica 02

--- ionic-basic/src/app/presupuesto/presupuesto.page.ts
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Gasto } from '../models/gastos.model';
+import { GastosService } from '../services/gastos.service';
 
 @Component({
   selector: 'app-presupuesto',
@@ -12,12 +14,16 @@ export class PresupuestoPage implements OnInit {
   public monto: number;
   public resultados: string;
   public errResultados: string='light';
+  
+  public gasotsList: Gasto[]=[];
+  public descripcion: string;
+  public tipoGasto: string='';
 
-  constructor() { 
+  constructor(private gastosService: GastosService) {
     
-  }
+   }
 
-customPopoverOptions: any = {
+   customPopoverOptions: any = {
     header: 'Seleccion de gasots',
     subHeader: 'Seleccione el tipo de gasto',
     message: 'Solo seleccione un tipo de gasto'
@@ -29,9 +35,17 @@ customPopoverOptions: any = {
   
   guardar(){
     this.resultados ="";
-    if(this.monto!=null && this.selectedValue!=null){
+    if(this.monto!=null && this.selectedValue!=null && this.descripcion!= null){
       this.errResultados = 'success';
-      this.resultados = 'Gasto seleccionado: '+this.selectedValue+' \nMonto: '+this.monto+'\n';
+      this.resultados = 'Gasto seleccionado: '+this.selectedValue+' \nMonto: '+this.monto+'\n'+
+      'Descricion: '+this.descripcion;
+      let gasto: Gasto = {
+      descripcion: this.descripcion,
+      tipo: this.selectedValue,
+      monto: this.monto
+    }
+      this.gastosService.agregar(gasto);
+      this.gasotsList = this.gastosService.getGastos();
     }
     else{
       this.errResultados = 'danger';
@@ -39,6 +53,10 @@ customPopoverOptions: any = {
     }
   }
 
+  borrarGasto(idGasto: number){
+    this.gastosService.borrarGasto(idGasto);
+    this.gasotsList = this.gastosService.getGastos();
+  }
 
   ngOnInit() {
   }
