63309e1bcb603d7f208cfcdf1846e1e45fd9be82
Fran22Espinoza <fespinozagal@gmail.com>
2022-03-03 18:15:36 -0600

    practica 04

---
+++ ionic-basic/src/app/reciever/reciever.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+import { Personaje } from "../models/personaje.models";
+import { SerciceCService } from "../services/sercice-c.services";
+
+
+@Component({
+  selector: 'app-reciever',
+  templateUrl: './reciever.page.html',
+  styleUrls: ['./reciever.page.scss'],
+})
+export class RecieverPage implements OnInit {
+
+  user: any;
+  list: any[];
+  personajes: Personaje[];
+  
+  constructor(private service: SerciceCService) { }
+
+  ngOnInit() {
+    this.service.$getObjectSource.subscribe(data=>{
+      console.log(data);
+      this.user = data;
+    }).unsubscribe();
+    this.service.$getListSource.subscribe(data=>{
+      console.log(data);
+      this.list = data;
+    }).unsubscribe();
+
+    this.service.getPersonajes().subscribe((response: any)=>{
+      this.personajes = response.results;
+    });
+  }
+
+}
