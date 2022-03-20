b843dfc8b05c97ac67cec01ee574bb9933d734ef
bgr8594 <bgr8594@gmail.com>
2022-03-20 16:00:24 -0600

    practica 08, Validacion de formularios, estilos globales scss

--- ionic-basic/src/app/login/login.page.ts
+++ ionic-basic/src/app/login/login.page.ts
@@ -4,7 +4,7 @@ import { AutserviceService } from '../services/autservice.service';
 import { ModalErrorComponent } from '../componentes/modal-error/modal-error.component';
 import { ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
-
+import {FormGroup, FormBuilder, Validators, FormControl, AbstractControl} from '@angular/forms';
 
 
 @Component({
@@ -15,11 +15,14 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
 
   user: User = new User();
+  ionicForm: FormGroup;
+
   constructor(
     private router: Router,private autSvc: AutserviceService,
-    private modalCtrl: ModalController,) { }
+    private modalCtrl: ModalController,private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
   async onLogin(){
@@ -47,4 +50,31 @@ export class LoginPage implements OnInit {
     return await modal.present();
   }
 
+  buildForm(){
+    this.ionicForm = this.formBuilder.group({
+      email: new FormControl('',{validators: [Validators.email,Validators.required]}),
+      password: new FormControl('', {validators: [Validators.required, Validators.minLength(6), Validators.maxLength(6)]})
+    });
+  }
+  hasError: any = (controlName: string, errorName: string) => {
+		return !this.ionicForm.controls[controlName].valid &&
+			this.ionicForm.controls[controlName].hasError(errorName) &&
+			this.ionicForm.controls[controlName].touched;
+	}
+
+
+	notZero(control: AbstractControl) {
+		if (control.value && control.value.monto <= 0) {
+			return { 'notZero': true };
+		}
+		return null;
+	} 
+  
+  submitForm(){
+    if(this.ionicForm.valid){
+      this.user.email = this.ionicForm.get('email').value;
+      this.user.password = this.ionicForm.get('password').value;
+      this.onLogin();
+    }
+  }
 }
