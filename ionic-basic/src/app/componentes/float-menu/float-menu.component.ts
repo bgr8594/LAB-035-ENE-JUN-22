60134d1ea31b108108da5499a28a0e31bee63a28
Fran22Espinoza <fespinozagal@gmail.com>
2022-03-25 12:03:01 -0600

    practica 07

--- ionic-basic/src/app/componentes/float-menu/float-menu.component.ts
+++ ionic-basic/src/app/componentes/float-menu/float-menu.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthGuard } from 'src/app/guards/auth.guard';
+import { MenuElement } from 'src/app/models/menu.model';
+import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-float-menu',
@@ -7,8 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FloatMenuComponent implements OnInit {
 
-  constructor() { }
+  titleMenu: string='home';
 
-  ngOnInit() {}
+  public isLoged : any = false;
 
+  datosMenu: MenuElement[] =[
+    {nombre: 'login',enlace:'/login',
+    icono:'log-in-outline'},
+    {nombre: 'logout',enlace:'/home',
+    icono:'log-out-outline'}
+  ];
+  
+  constructor(private router: Router, private authGuard: AuthGuard,
+    private menuService: MenuService,
+    private afAuth: AngularFireAuth) { }
+
+  ngOnInit() {
+    this.afAuth.authState.subscribe(user => this.isLoged= user);
+
+    this.menuService.$getTitleMenu.subscribe(data=>{
+      console.log(data);
+      this.titleMenu =data;
+    });
+  }
+
+  navegar(link: string, titleMenu: string){
+    this.titleMenu =titleMenu;
+    this.router.navigate([link]);
+  }
+
+  onMenuOpen(){
+    if(this.isLoged){
+      this.datosMenu =[
+        {nombre: 'Alumnos',enlace:'/alumnos',
+  icono:'school-outline'},
+    {nombre: 'Receteas',enlace:'/receta',
+    icono:'restaurant-outline'},
+    {nombre: 'Presupuesto',enlace:'/presupuesto',
+    icono:'cash-outline'},
+    {nombre: 'inicio',enlace:'/inicio',
+    icono:'navigate-outline'},
+    {nombre: 'Tabs',enlace:'/tabs',
+    icono:'folder-outline'},
+    {nombre: 'login',enlace:'/login',
+    icono:'log-in-outline'},
+        {nombre: 'logout',enlace:'/home',
+        icono:'log-out-outline'}
+      ];
+      
+    } else{
+      this.datosMenu =[
+        {nombre: 'login',enlace:'/login',
+        icono:'log-in-outline'},
+        {nombre: 'logout',enlace:'/home',
+        icono:'log-out-outline'}
+      ];
+    }
+  }
 }
