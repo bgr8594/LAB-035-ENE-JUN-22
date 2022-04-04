acff6a9e9339fd1b0e77e83977ee71f122f338fc
KellyHdz <kelly.hernandezvqz@uanl.edu.mx>
2022-04-03 23:49:19 -0500

    practica 04

--- ionic-basic/src/app/reciever/reciever.page.ts
+++ ionic-basic/src/app/reciever/reciever.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../models/personaje.model';
+import { servicioCService } from '../services/servicio-c.service';
 
 @Component({
   selector: 'app-reciever',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecieverPage implements OnInit {
 
-  constructor() { }
+  user: any;
+  list: any[];
+  personajes: Personaje[];
+
+  constructor(private service: servicioCService) { }
 
   ngOnInit() {
+    this.service.$getObjectSource.subscribe(data=>{
+      console.log(data);
+      this.user = data;
+    }).unsubscribe();
+    this.service.$getListSource.subscribe(data=>{
+      console.log(data);
+      this.list = data;
+    }).unsubscribe();
+
+    this.service.getPersonajes().subscribe((response: any)=>{
+      this.personajes = response.results;
+    });
   }
 
 }
