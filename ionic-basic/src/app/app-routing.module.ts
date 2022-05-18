69a6510e97625714c37e7a1c189fa458ed53ab44
WinningPiano33 <milthon2012ac@hotmail.com>
2022-05-18 00:13:40 -0500

    practica 13

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -64,6 +64,10 @@ const routes: Routes = [
     path: 'googlemaps',
     loadChildren: () => import('./googlemaps/googlemaps.module').then( m => m.GooglemapsPageModule)
   },
+  {
+    path: 'galeria',
+    loadChildren: () => import('./galeria/galeria.module').then( m => m.GaleriaPageModule)
+  },
 ];
 
 @NgModule({
