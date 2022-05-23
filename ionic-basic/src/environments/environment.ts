eec17a74e6d292680839020b6b2be0d0a2634db2
Fran22Espinoza <fespinozagal@gmail.com>
2022-05-22 22:01:10 -0500

    practica 09 y 10

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -3,14 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCkpMeJBqhhXJ47JWjtadxPyo0KMypPaws",
-    authDomain: "ionic-basic-035.firebaseapp.com",
-    projectId: "ionic-basic-035",
-    storageBucket: "ionic-basic-035.appspot.com",
-    messagingSenderId: "151373407537",
-    appId: "1:151373407537:web:9c50e2e729e61829547c10"
+  
+  apiKeyMaps: "AIzaSyAxaJ4Lf0Wbnn1wOOlt_cGzBtFsk1eIrcc",
+  firebaseConfig:{
+  apiKey: "AIzaSyD-PD0mGsF9GxLwlfczFO8jQPy6wOST6bI",
+  authDomain: "ionic-basic-6422b.firebaseapp.com",
+  projectId: "ionic-basic-6422b",
+  storageBucket: "ionic-basic-6422b.appspot.com",
+  messagingSenderId: "927492699179",
+  appId: "1:927492699179:web:61774a9fd85e1902f0a66e",
+  measurementId: "G-X8D38KZMKT"
   }
 };
 
